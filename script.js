e35b0277c07462302f6bbef05475bc6517dff3cd
luca-eweb <luca.luedi@students.bfh.ch>
2024-12-08 14:19:36 +0100

    Update Header

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 
-
-
 function toggleMenu() {
     const menu = document.getElementById('mobile-menu');
     menu.classList.toggle('active');
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('hamburger-menu').addEventListener('click', toggleMenu);
+});
